41f0cdfc48d84760930b6587fd4eabbc60fb3b2d
Scott Young <youngscottj@gmail.com>
2020-04-07 18:01:12 -0600

    initial commit

---
+++ TripleX.cpp
@@ -0,0 +1,11 @@
+// TripleX.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "You are breaking into the server room";
+	std::cout << "\nEnter the correct code or parish";
+	return 0;
+}
